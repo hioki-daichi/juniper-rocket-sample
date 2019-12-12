9356f917465a408bd9cd631eccf16d79056de4d4
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-12 23:50:25 +0900

    Use VideoDecorator

--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -1,3 +1,4 @@
+use crate::video::decorator::VideoDecorator;
 use crate::video::model::Video;
 use crate::Context;
 use juniper::{FieldError, FieldResult, RootNode, Value};
@@ -5,8 +6,8 @@ use juniper::{FieldError, FieldResult, RootNode, Value};
 pub struct Query;
 
 graphql_object!(Query: Context |&self| {
-    field videos(&executor) -> FieldResult<Vec<Video>> {
-        Video::all(executor.context()).map_err(|e| FieldError::new(e, Value::null()))
+    field videos(&executor) -> FieldResult<Vec<VideoDecorator>> {
+        Video::all(executor.context()).map(|videos| videos.iter().map(|video|VideoDecorator::from(&video)).collect()).map_err(|e| FieldError::new(e, Value::null()))
     }
 });
 
