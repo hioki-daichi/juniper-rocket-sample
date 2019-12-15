37d4835da3475b22ffef38f3aa7302eea9e3b048
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-15 16:26:17 +0900

    Add fn: util::s3::put_object

--- src/util/s3.rs
+++ src/util/s3.rs
@@ -1,7 +1,27 @@
 use rusoto_core::credential::AwsCredentials;
-use rusoto_core::Region;
+use rusoto_core::{ByteStream, Region};
 use rusoto_s3::util::{PreSignedRequest, PreSignedRequestOption};
-use rusoto_s3::GetObjectRequest;
+use rusoto_s3::{GetObjectRequest, PutObjectRequest, S3Client, S3};
+
+pub fn put_object(bucket: String, key: String, data: Vec<u8>) {
+    let client = S3Client::new(get_region());
+
+    let body = Some(ByteStream::from(data));
+    let content_type = Some("video/mp4".to_owned());
+
+    let output = client
+        .put_object(PutObjectRequest {
+            bucket,
+            key,
+            body,
+            content_type,
+            ..Default::default()
+        })
+        .sync()
+        .unwrap();
+
+    println!("{:?}", output);
+}
 
 pub fn get_presigned_url(bucket: String, key: String) -> String {
     GetObjectRequest {
